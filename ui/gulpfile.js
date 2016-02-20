1dcbbe78bad20067534f3edf55a1a06d55abb55e
Robert Fuszenecker <robert.fuszenecker@outlook.com>
2016-02-20 22:45:03 +0100

    Added gulp

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,1 +1,12 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
+gulp.task('run', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      fallback: '/index.html'
+    }));
+});
