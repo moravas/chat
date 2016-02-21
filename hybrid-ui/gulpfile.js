4ef401505bb8601fbdd1543156e7e147aaecf3ba
Robert Fuszenecker <robert.fuszenecker@outlook.com>
2016-02-21 12:56:26 +0100

    Added test framework

--- hybrid-ui/gulpfile.js
+++ hybrid-ui/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var qunit = require('gulp-qunit');
 
 gulp.task('run', function() {
   gulp.src('.')
@@ -9,4 +10,9 @@ gulp.task('run', function() {
       open: true,
       fallback: '/index.html'
     }));
+});
+
+gulp.task('web-tests', function() {
+    return gulp.src('./tests/login.html')
+        .pipe(qunit());
 });
