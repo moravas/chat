bb3bdeab6f0746c717157c8efcface8468e7f3e3
norbert_toth <norbert.toth@nuance.com>
2016-02-24 08:20:22 +0100

    Add Conversation user story

--- doc/userstory.hpp
+++ doc/userstory.hpp
@@ -10,12 +10,6 @@
 //! \tableofcontents
 //! \section userstory User stories
 //!
-//! \subsection US001 US001: Conversation
-//! <b>Affected features:</b> <a href="#FT006">FT006</a><br>
-//! The process of the conversation between 2 particiapants can be splitted up to three parts regardless of the state of the participants:
-//!     -# <b>Initiate conversation:</b>
-//!     .
-//!
 //! \subsection US002 US002: Request authentication
 //! <b>Affected features:</b> <a href="#FT002">FT002</a><br>
 //! As an unauthenticated user, I'd like to sign in through the REST API by using HTTP POST in order to send my user name and password and receive 
@@ -198,6 +192,72 @@
 //!         CONSTRAINT message_user_switch_message_fk FOREIGN KEY (message) REFERENCES conversations (key),
 //!         CONSTRAINT message_user_switch_user_fk FOREIGN KEY (user) REFERENCES users (id));
 //!     \endcode
+//!
+//! \subsection US006 US006: Conversation manager
+//! <b>Affected features:</b> <a href="#FT006">FT006</a><br>
+//! Conversation messages are based on JSON represented text messages. They can play both of command and message roles. For example, inviting a new
+//! participant can be done by the message:
+//! \code
+//!     { "invite":[ {"username":"user"} ]}
+//! \endcode
+//! or:
+//! \code
+//!     { "id": "124", "invite":[ {"username":"user"} ]}
+//! \endcode
+//! The examples above are similar. The only difference is that the first one creates a new session as well otherwise invites the user "user" into
+//! the session labeled by session ID "124". Invited users are represented as array even if only one user were invited. Closing a session is very
+//! similar:
+//! \code
+//!     { "id": "124", "close":"user" }
+//! \endcode
+//! Receiving the close message only accepted form the client who wants to exit from a particular session. Excluding another user is prohibited. The
+//! server should be prepared to prevent such a cases because they assume attack against to the server or another clients. If there are no more clients
+//! in the current session except the sender one, the Session listener will send back an empty session message:
+//! \code
+//!     { "id": "124", "property":"empty" }
+//! \endcode
+//! In this case, it is up to the client to discard the message or re-invite the participants and sends the message again.
+//! The messages above are command which targets the server to perform some operations on it, but for messaging client around the word should be
+//! forward the incoming message.
+//!
+//! The text message looks like this:
+//! \code
+//!     {
+//!     "id": "124",
+//!     "sender":"user",
+//!     "message": "arbitrary text message"
+//!     }
+//! \endcode
+//! It can be seen that the message doesn't holds any information the addresses, but the originator is present.
+//!
+//! \subsubsection initiate_conversation Initiate conversation
+//! After the client logged on to the server, the client is responsible for opening a new socket. This socket will be registered by Conversation
+//! manager identified by the username. If the user sends an invitation message to the server, the Conversation manager looks for the session ID in
+//! the message and use it or create a new one and checks whether a connection exists identified by the username.
+//!     -# If there were no session ID in the invitation message, the Conversation manager deploys new Session Listener and provides to it a list
+//!         containing the username and sockets. If it is done, the Conversation manager sends back the session ID to the client.
+//!     -# If the session already exists, the session manager appends the list (at least one item) of invited username and socket pairs to the
+//!         Session listener.
+//!     .
+//! \note The socket could be NULL, in this case the client sends off-line messages that could be queried by the conversation partner it will be
+//! back again.
+//!
+//! \subsubsection conversation Conversation
+//! If the new message received by the Conversation manager, it looks for a Session listener specified by the session ID and forward the message to it.
+//! The Session listener perform the following operations:
+//!     -# Persist the message into the "conversations" table
+//!     -# Persist insert the message ID and the ID of addressed users into the "message_user_switch" table
+//!     -# Walk trough its listeners and forwards the message to them except the sender and off-line clients
+//!     .
+//! If any error occurred during these operations, the Session listener will hang on its activity and returns to the Conversation manager.
+//!
+//! \subsubsection close_conversation Close conversation
+//! If the client wants to exit from a conversation, sends the "close" command to the server. In this case the socket itself still alive, but the
+//! Session listener unregisters the particular client. After exiting from the session, the client doesn't receive messages from clients in that
+//! session. Re-invite the client is possible into the session.
+//!
+//! If the client closes the socket as well, the Conversation manager is responsible for unregister the socket from the Session listeners and persist
+//! the log off time-stamp into the "users" table.
 //!
 // =============================================================================
 //! \file
