b39f5e65f0af28a2d9366add00ad2598f837ee1e
norbert_toth <norbert.toth@nuance.com>
2016-02-25 10:49:53 +0100

    Test implementation of Accountmanager controller.

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace server
 {
@@ -10,6 +7,12 @@ namespace server
     {
         static void Main(string[] args)
         {
+            string baseUri = "http://localhost:8080";
+
+            Console.WriteLine("Starting web Server...");
+            WebApp.Start<WebApiConfig>(baseUri);
+            Console.WriteLine("Server running at {0} - press Enter to quit. ", baseUri);
+            Console.ReadLine();
         }
     }
 }
---
+++ server/server/REST/Controller/Accountmanager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace server
+{
+    [Route("users")]
+    class AccountManager : ApiController
+    {
+        [Route("")]
+        [HttpDelete]
+        public void DeleteUserAccount()
+        {
+            string headerValues = Request.Headers.GetValues("MyCustomID").FirstOrDefault();
+            Console.WriteLine(headerValues);
+        }
+
+        [Route("")]
+        [HttpGet]
+        public void Login()
+        {
+            Console.WriteLine(Request.Content);
+        }
+
+        [Route("/register")]
+        [HttpPost]
+        public void Register()
+        {
+            Console.WriteLine(Request.Content);
+        }
+    }
+}
