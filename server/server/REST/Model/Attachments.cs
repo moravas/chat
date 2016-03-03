98066dcd8943e233b058b62a396b2afa0cf6d688
moravas <moravas8051@gmail.com>
2016-03-03 20:39:19 +0100

    Add draft of models

---
+++ server/server/REST/Model/Attachments.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server
+{
+    public class Attachment
+    {
+        [StringLength(256, ErrorMessage = "Attachment name length exceds the maximum.")]
+        public string Name { get; set; }
+
+        public byte[] Content { get; set; }
+
+        public uint UserID { get; set; }
+    }
+}
---
+++ server/server/REST/Model/MessageUserSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server
+{
+    public class MessageUserSwitch
+    {
+        [Key]
+        public ulong MessageID { get; set; }
+
+        public uint UserID { get; set; }
+    }
+}
