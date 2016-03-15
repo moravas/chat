bc3c752c7db8b5971b6e15bd7474b255cb737301
moravas <moravas8051@gmail.com>
2016-03-15 16:43:22 +0100

    Add unit test to server project

---
+++ server/ServerTest/UserModelTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Server;
+
+namespace ServerTest
+{
+    [TestClass]
+    public class UserModelTest
+    {
+        private UsersDB db;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            db = new UsersDB();
+            db.Database.CreateIfNotExists();
+            db.Users.Create();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            db.Database.Delete();
+        }
+
+        [TestMethod]
+        public void InsertValidUser()
+        {
+            db.Users.Add(GetPreparedUser());
+            db.SaveChanges();
+            foreach(User user in db.Users)
+            {
+                Assert.AreEqual(user.ID, 0);
+                Assert.AreEqual(user.UserName, "ValidBob");
+                Assert.AreEqual(user.Email, "validbob@email.com");
+                Assert.AreEqual(user.Password, "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567");
+                Assert.AreEqual(user.Logoff, null);
+            }
+        }
+
+        private User GetPreparedUser()
+        {
+            return new User()
+            {
+                UserName = "ValidBob",
+                Email = "validbob@email.com",
+                Password = "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567"
+            };
+        }
+    }
+}
