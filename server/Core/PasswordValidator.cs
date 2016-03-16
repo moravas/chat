e83b24247f137fcf988775c5424b1efdc931a14a
moravas <moravas8051@gmail.com>
2016-03-16 20:51:10 +0100

    Add Core project

---
+++ server/Core/PasswordValidator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core
+{
+    public class ValidatorStatement
+    {
+        public int ExpectedAtLeast { get; set; }
+        public IEnumerable<char> Data { get; set; }
+    }
+
+    public class PasswordValidator
+    {
+        public int MinLenght { get; set; }
+        public List<ValidatorStatement> Statements { get; set; }
+        public bool Validate(IEnumerable<char> data)
+        {
+            foreach(var s in Statements)
+            {
+                if(data.Intersect(s.Data).Count() < s.ExpectedAtLeast)
+                {
+                    return false;
+                }
+            }
+
+            return true && data.Count() >= MinLenght;
+        }
+    }
+}
